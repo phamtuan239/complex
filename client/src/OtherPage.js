12d0c67922af1a592c559ad2eb92ba8a675bc64c
minggu99 <tuanphamtran99@gmail.com>
2022-07-21 13:45:11 +0700

    create complex 19

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 const OtherPage = () => {
   return (
-    <div>
+    <Fragment>
       Im some other page!
       <Link to="/">Go back home</Link>
-    </div>
+    </Fragment>
   )
 }
 
